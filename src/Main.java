309f7abb944337a4096b929364d37370824c30fb
Adekri <adekri@protonmail.com>
2023-12-01 19:39:23 +0100

    comm

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
